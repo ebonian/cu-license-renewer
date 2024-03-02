5b7762ef4d82a99c78973797b734e7a1895db186
ebonian <tanadon.santisan@gmail.com>
2024-03-02 15:45:06 +0700

    fix: build workflow

--- main.py
+++ main.py
@@ -2,18 +2,18 @@ import json
 import os
 from renew import init_driver, borrow
 
+USERNAME = os.environ['USERNAME']
+PASSWORD = os.environ['PASSWORD']  
+
 def main():
     f = open('software.json')
     data = json.load(f)
 
-    USERNAME = os.environ['USERNAME']
-    PASSWORD = os.environ['PASSWORD']  
-
     driver = init_driver() 
 
-    for i in data:
-        borrow(driver, USERNAME, PASSWORD, i['value'])
-        print(f'Successfully borrowed {i["name"]}')
+    for software in data:
+        borrow(driver, USERNAME, PASSWORD, software['value'])
+        print(f'Successfully borrowed {software["name"]}')
 
     driver.quit()
 
